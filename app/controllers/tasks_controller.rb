58c7f6ff1183dee4f3574df109d3a6875a5fc043
kazuhei.nagai <ivish2255@gmail.com>
2020-11-28 12:43:10 +0000

    kadai9.7

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -52,6 +52,8 @@ class TasksController < ApplicationController
     redirect_to tasks_url
   end
   
+  private
+  
     # Strong Parameter
   def task_params
     params.require(:task).permit(:content,:status)
